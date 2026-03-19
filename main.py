ad80d01cb3a9359cdd415df6201fcaed1cdec1f7
hanydabous <hanyyy.dabous@gmail.com>
2026-03-19 11:41:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -95,11 +95,3 @@ def health():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
-```
-
-Also update `requirements.txt`:
-```
-flask
-yt-dlp
-requests
-boto3
